cb55a66a506f96455ac7984a8432a3c3f2a4c9fd
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-16 10:28:16 -0500

    Tweaking rspec documentation
    [skip ci]

--- spec/lib/democritus_spec.rb
+++ spec/lib/democritus_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Democritus do
   its(:constants) { should include(:DemocritusObjectTag) }
   its(:build) { should be_a(Class) }
 
-  context 'an object instantiated from the built class' do
+  context 'an object instantiated from Democritus.build' do
     subject { described_class.build.new }
     it 'should be a DemocritusObjectTag' do
       expect(subject).to be_a(Democritus::DemocritusObjectTag)
