330bb885f3b0adf961c3a1a57fb76b1889052058
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-16 19:17:22 -0500

    Removing excess lines from tests

--- spec/lib/democritus/class_builder_spec.rb
+++ spec/lib/democritus/class_builder_spec.rb
@@ -13,22 +13,14 @@ RSpec.describe Democritus::ClassBuilder do
     end
 
     it 'will add instance methods defined in the block to the instance methods of the customization module' do
-      expect do
-        subject.customize do |builder|
-          def hello_world
-          end
-        end
-      end.to change { subject.customization_module.instance_methods.include?(:hello_world) }.from(false).to(true)
+      expect { subject.customize { |builder| def hello_world; end } }.
+        to change { subject.customization_module.instance_methods.include?(:hello_world) }.from(false).to(true)
       expect(subject.customization_module.methods).to_not include(:hello_world)
     end
 
     it 'will add class methods defined in the block to the class methods of the customization module' do
-      expect do
-        subject.customize do |builder|
-          def self.hello_world
-          end
-        end
-      end.to change { subject.customization_module.methods.include?(:hello_world) }.from(false).to(true)
+      expect { subject.customize { |builder| def self.hello_world; end } }.
+        to change { subject.customization_module.methods.include?(:hello_world) }.from(false).to(true)
       expect(subject.customization_module.instance_methods).to_not include(:hello_world)
     end
   end
