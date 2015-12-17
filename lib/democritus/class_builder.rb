96a116954d60202df2708e8026fdafbc660e8686
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-12-16 19:36:23 -0500

    Adding command handling to ClassBuilder

--- lib/democritus/class_builder.rb
+++ lib/democritus/class_builder.rb
@@ -23,5 +23,30 @@ module Democritus
         include DemocritusObjectTag
       end
     end
+
+    private
+
+    def method_missing(method_name, *args, &block)
+      command_name = command_name_for_method(method_name)
+      if Commands.const_defined?(command_name)
+        command_class = Commands.const_get(command_name)
+        command_class.new(self, *args, &block).call
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      respond_to_definition(method_name, include_private, :respond_to_missing?)
+    end
+
+    def respond_to_definition(method_name, include_private, respond_to_method_name)
+      command_name = command_name_for_method(method_name)
+      Commands.const_defined?(command_name)
+    end
+
+    def command_name_for_method(method_name)
+      method_name.to_s.gsub(/(?:^|_)([a-z])/) { Regexp.last_match[1].upcase }
+    end
   end
 end
--- spec/lib/democritus/class_builder_spec.rb
+++ spec/lib/democritus/class_builder_spec.rb
@@ -1,27 +1,54 @@
 require 'spec_helper'
 require 'democritus/class_builder'
 
-RSpec.describe Democritus::ClassBuilder do
-  subject { described_class.new }
+module Democritus
+  class ClassBuilder
+    module Commands
+      class TestCommand
+      end
+    end
+  end
 
-  its(:generate_class) { should be_a(Class) }
-  its(:customization_module) { should be_a(Module) }
+  RSpec.describe ClassBuilder do
+    subject { described_class.new }
 
-  context '#customize' do
-    it 'will return nil if no block is passed' do
-      expect(subject.customize).to be_nil
+    its(:generate_class) { should be_a(Class) }
+    its(:customization_module) { should be_a(Module) }
+
+    it 'responds to commands defined in ClassBuilder::Commands' do
+      expect(subject).to respond_to(:test_command)
     end
 
-    it 'will add instance methods defined in the block to the instance methods of the customization module' do
-      expect { subject.customize { |builder| def hello_world; end } }.
-        to change { subject.customization_module.instance_methods.include?(:hello_world) }.from(false).to(true)
-      expect(subject.customization_module.methods).to_not include(:hello_world)
+    it 'translates method calls into command invocations including arguments' do
+      test_command = double
+      expect(described_class::Commands::TestCommand).to receive(:new).
+        with(subject, 'foo', 42).
+        and_return(test_command)
+      expect(test_command).to receive(:call).and_return('COMMAND RESULT')
+      expect(subject.test_command('foo', 42)).to eq('COMMAND RESULT')
     end
 
-    it 'will add class methods defined in the block to the class methods of the customization module' do
-      expect { subject.customize { |builder| def self.hello_world; end } }.
-        to change { subject.customization_module.methods.include?(:hello_world) }.from(false).to(true)
-      expect(subject.customization_module.instance_methods).to_not include(:hello_world)
+    it 'handles missing non-command missing methods normally' do
+      expect(subject).not_to respond_to(:nonexistant_method)
+      expect { subject.nonexistent_method }.to raise_error(NoMethodError)
+    end
+
+    context '#customize' do
+      it 'will return nil if no block is passed' do
+        expect(subject.customize).to be_nil
+      end
+
+      it 'will add instance methods defined in the block to the instance methods of the customization module' do
+        expect { subject.customize { |builder| def hello_world; end } }.
+          to change { subject.customization_module.instance_methods.include?(:hello_world) }.from(false).to(true)
+        expect(subject.customization_module.methods).to_not include(:hello_world)
+      end
+
+      it 'will add class methods defined in the block to the class methods of the customization module' do
+        expect { subject.customize { |builder| def self.hello_world; end } }.
+          to change { subject.customization_module.methods.include?(:hello_world) }.from(false).to(true)
+        expect(subject.customization_module.instance_methods).to_not include(:hello_world)
+      end
     end
   end
 end
