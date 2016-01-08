b18e637e632fc49666210d228a1cb015272d98a2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-01-08 10:20:02 -0500

    Updating Commands::Attribute
    I want to document and enforce interface expectations.

--- lib/democritus/class_builder/commands/attribute.rb
+++ lib/democritus/class_builder/commands/attribute.rb
@@ -5,14 +5,21 @@ module Democritus
     module Commands
       # Command to assign an attribute to the given built class.
       class Attribute < ::Democritus::ClassBuilder::Command
+        # @param builder [#defer, Democritus::ClassBuilder]
+        # @param name [#to_sym] The name of the attribute
+        # @param options [Hash] The configuration options of the attribute
         def initialize(name:, **options)
           self.builder = options.fetch(:builder)
           self.name = name
           self.options = options
         end
 
-        attr_reader :name, :options
+        attr_reader :name
+        attr_reader :options
 
+        # Generate the code for the attribute
+        #
+        # @return void
         # :reek:NestedIterators: { exclude: [ 'Democritus::ClassBuilder::Commands::Attribute#call' ] }
         def call
           defer do |subject|
@@ -26,7 +33,11 @@ module Democritus
 
         private
 
-        attr_writer :name, :options
+        attr_writer :options
+
+        def name=(input)
+          @name = input.to_sym
+        end
       end
     end
   end
