a7f311afeaec2ad6842ee5962fa7d92ac09ad120
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-01-08 10:20:02 -0500

    Fixing documented parameter name
    [skip ci]

--- lib/democritus/class_builder.rb
+++ lib/democritus/class_builder.rb
@@ -119,7 +119,7 @@ module Democritus
     #
     # @param [Hash] options
     # @option options [Boolean] :prepend Is there something about this deferred_operation that should happen first?
-    # @param defered_operation [#call] The operation that will be applied to the generated class
+    # @param deferred_operation [#call] The operation that will be applied to the generated class
     #
     # @return void
     def defer(**options, &deferred_operation)
